159091f1eec9fb9a0ece68bbba3c91611ba3a4eb
rushi20139 <87067394+rushi20139@users.noreply.github.com>
2023-01-22 08:35:18 +0530

    Create ParticipantDto.java
    Data Transfer Object for Participant Class

---
+++ ParticipantDto.java
@@ -0,0 +1,61 @@
+package com.babylogging.babybackend.payloads;
+
+
+public class ParticipantDto {
+
+ private int pid;
+ private String pname; 
+ private float points;
+ private String timeTaken:
+ private float percentile;
+ private int rank;
+
+ public Integer getPid() { 
+ return pid;
+ }
+
+ public void setPid(int pid){
+  this.pid = id;
+ }
+
+ public String getPname() {
+  return pname;
+ }
+
+ public void setPname(String pname) {
+  this.pname = pname;
+ }
+
+ public float getPoints() {
+  return points;
+ }
+
+ public void setPoints(float points) {
+  this points=points;
+ } 
+
+ public String getTimeTaken() { 
+ return timeTaken;
+ }
+
+ public void set TimeTaken(String timeTaken){
+  this.timeTaken = timeTaken:
+ }
+
+public float getPercentile() {
+ return percentile;
+}
+
+ public void setPercentile(float percentile){
+ this.percentile=percentile;
+ }
+
+ public Integer getRank(){
+ return rank;
+ }
+
+ public Integer setRank(int rank){
+ this.rank=rank;
+ }
+
+}
